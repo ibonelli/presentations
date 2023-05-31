3b3d66bc79d88b13a408157f8772f3b9fd6949b3
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2023-05-31 11:40:56 -0300

    DevOps Presentation

---
+++ fetch_local.sh
@@ -0,0 +1,13 @@
+mkdir dist
+
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/reset.min.css -o ./dist/reset.min.css
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/reveal.min.css -o ./dist/reveal.min.css
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/theme/black.min.css -o ./dist/black.min.css
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/plugin/highlight/monokai.min.css -o ./dist/monokai.min.css
+
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/reveal.min.js -o ./dist/reveal.min.js
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/plugin/zoom/zoom.min.js -o ./dist/zoom.min.js
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/plugin/notes/notes.min.js -o ./dist/notes.min.js
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/plugin/search/search.min.js -o ./dist/search.min.js
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/plugin/markdown/markdown.min.js -o ./dist/markdown.min.js
+curl https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.5.0/plugin/highlight/highlight.min.js -o ./dist/highlight.min.js
